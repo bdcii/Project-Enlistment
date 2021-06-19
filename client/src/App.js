0d2d41418c47a2cbf309dfa2bf53cb3f29e123cd
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-19 08:04:10 -0400

    Make basic file changes to make React work

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,18 @@
 import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.js";
+
+function App() {
+    return (
+        <>
+            <Router>
+                <h1>Project Enlistment</h1>
+
+            </Router>
+        </>
+    )
+}
+
 
 export default App;
