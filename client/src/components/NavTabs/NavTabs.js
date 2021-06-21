7f1ba3afbd61e669fcbfa261f96f38ddf44ca64e
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-21 11:54:17 -0400

    Center page heading on the home page, correct route paths for NavTabs

--- client/src/components/NavTabs/NavTabs.js
+++ client/src/components/NavTabs/NavTabs.js
@@ -6,8 +6,8 @@ function NavTabs() {
     return (
       <div className="nav">
           <Link to="/"><button id="nav1">Home</button></Link>
-          <Link to="/work"><button id="nav2">Profile</button></Link>
-          <Link to="/work"><button id="nav3">Sign In / Sign Up</button></Link>
+          <Link to="/profile"><button id="nav2">Profile</button></Link>
+          <Link to="/signin"><button id="nav3">Sign In / Sign Up</button></Link>
       </div>
     )
 }
