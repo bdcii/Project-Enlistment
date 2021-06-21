38bc12e240c19621fbcbe52f504cbb7455b9fb0a
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-21 12:21:53 -0400

    Update order of navtabs

--- client/src/components/NavTabs/NavTabs.js
+++ client/src/components/NavTabs/NavTabs.js
@@ -6,8 +6,8 @@ function NavTabs() {
     return (
       <div className="nav">
           <Link to="/"><button id="nav1">Home</button></Link>
-          <Link to="/profile"><button id="nav2">Profile</button></Link>
-          <Link to="/signin"><button id="nav3">Sign In / Sign Up</button></Link>
+          <Link to="/signin"><button id="nav2">Sign In / Sign Up</button></Link>
+          <Link to="/profile"><button id="nav3">Profile</button></Link>
       </div>
     )
 }
