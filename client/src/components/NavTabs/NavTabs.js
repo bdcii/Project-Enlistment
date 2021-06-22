20e55966ad66c8835aa169adeaa0cf0c59c35b34
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-21 20:47:06 -0400

    Update margins, style, and color on the header and footer

--- client/src/components/NavTabs/NavTabs.js
+++ client/src/components/NavTabs/NavTabs.js
@@ -6,7 +6,7 @@ function NavTabs() {
     return (
       <div className="nav">
           <Link to="/"><button id="nav1">Home</button></Link>
-          <Link to="/signin"><button id="nav2">Sign In / Sign Up</button></Link>
+          <Link to="/signin"><button id="nav2">Login</button></Link>
           <Link to="/profile"><button id="nav3">Profile</button></Link>
       </div>
     )
