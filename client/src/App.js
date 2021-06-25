078294e61e926b67886ebbb8db10a96a8223dc9c
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-06-25 11:47:39 -0400

    working on authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs/NavTabs.js";
 import Home from "./pages/Home/Home.js";
 import Signin from "./pages/Signin/Signin.js";
 import Profile from "./pages/Profile/Profile.js";
+// import Login from './components/Login/Login';
+// import useToken from './components/App/useToken';
 // import Project from "./pages/Project/dashboard/Project"
 
+
+
 function App() {
+    // const {token, setToken} = useToken();
+
+    // if(!token) {
+    //     return <Login setToken={setToken} />
+    //   }
     return (
         <>
             <Router>
