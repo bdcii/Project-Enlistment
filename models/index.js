a9e9ee154b954bbc58b9854756fe537c9f7227ee
HRush <hrkoren@gmail.com>
2021-06-24 21:38:13 -0400

    worked on models and seed info based on skills/comments

--- models/index.js
+++ models/index.js
@@ -1,3 +1,14 @@
+const User = require('./User');
+const Project = require('./Project');
+
+Project.hasOne(User, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Project, {
+  foreignKey: 'project_id'
+});
+
 module.exports = {
   Project: require("./Project"),
   User: require("./User")
---
+++ models/legacy_Skills.js
@@ -0,0 +1,16 @@
+// Do we need this?
+
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const skillsSchema = new Schema(
+//     {
+//         skill_set: {
+//             type: String,
+//             required: true
+//         }
+//     });
+
+// const Skills = mongoose.model("Skills", skillsSchema);
+
+// module.exports = Skills;
