8bb6829feb8e61aafde0fb6d36aaa8ff8aa0b1f7
Kyle Euman <euman_kyle@yahoo.com>
2021-06-26 12:53:13 -0400

    changes to signup form and project form

--- client/src/pages/Project/newProject/ProjectForm.js
+++ client/src/pages/Project/newProject/ProjectForm.js
@@ -14,41 +14,51 @@ import "./ProjectForm.css";
 
    // }
         
-        class ProjectForm extends React.Component {
-            constructor(props) {
-              super(props);
-              this.state = {
-                value: 'Project Description'
-              };
-          
-              this.handleChange = this.handleChange.bind(this);
-              this.handleSubmit = this.handleSubmit.bind(this);
-            }
-          
-            handleChange(event) {
-              this.setState({value: event.target.value});
-            }
-          
-            handleSubmit(event) {
-              alert('Your Project was created!: ' + this.state.value);
-              event.preventDefault();
-            }
-           
-            render() {
-              return (
-                <form onSubmit={this.handleSubmit}>
-                  <label>
-                    Project Creation:
-                    <input type="text" value="Project Name"></input>
-                    <textarea value={this.state.value} onChange={this.handleChange} />
-                  </label>
-                  <input type="submit" value="Submit" />
-                </form>
-              );
-            }
-          }
-        
-          
+   class ProjectForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isGoing: true,
+        numberOfGuests: 2
+      };
+  
+      this.handleInputChange = this.handleInputChange.bind(this);
+    }
+  
+    handleInputChange(event) {
+      const target = event.target;
+      const value = target.type === 'checkbox' ? target.checked : target.value;
+      const name = target.name;
+  
+      this.setState({
+        [name]: value
+      });
+    }
+  
+    render() {
+      return (
+        <form>
+          <label>
+            Is going:
+            <input
+              name="isGoing"
+              type="checkbox"
+              checked={this.state.isGoing}
+              onChange={this.handleInputChange} />
+          </label>
+          <br />
+          <label>
+            Number of guests:
+            <input
+              name="numberOfGuests"
+              type="number"
+              value={this.state.numberOfGuests}
+              onChange={this.handleInputChange} />
+          </label>
+        </form>
+      );
+    }
+  }
         
     
 
--- client/src/pages/Signin/Signin.js
+++ client/src/pages/Signin/Signin.js
@@ -1,30 +1,83 @@
-import React from "react";
+import React, {useState} from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function Signin() {
-    return (
-        <>
-            <hr />
-            <h2>Sign In</h2>
-            <p>Input Email</p>
-            <p>Input Password</p>
-            <div className="button">
-                <button>Login</button>
-            </div>
-            <br />
-            <hr />
-            <h2>Create Login</h2>
-            <p>Input First Name</p>
-            <p>Input Last Name</p>
-            <p>Input Email</p>
-            <p>Input Password</p>
-            <div className="button">
-                <button>Create Account</button>
-            </div>
-            <br />
-            <hr />
-            <br />
-        </>
-    )
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
 }
+const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+function SignUp() {
+  return (
+    <div className="SignUp">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Create Account
+        </Button>
+      </Form>
+    </div>
+  );
+  }
 
 export default Signin;
