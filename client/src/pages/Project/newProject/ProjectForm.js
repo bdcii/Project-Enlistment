06539c6428941b26e3d4b0f1e1067fbed2b51f04
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-07-06 18:23:39 -0400

    Add basic styling, font, color to ProjectForm

--- client/src/pages/Project/newProject/ProjectForm.js
+++ client/src/pages/Project/newProject/ProjectForm.js
@@ -52,89 +52,91 @@ function ProjectForm() {
         <form>
             <div className="Container-1">
                 <div className="form-group text-left">
-                    <label htmlFor="ProjectName">Project Name</label>
-                    <input type="Text"
+                    <div className="projName"><label htmlFor="ProjectName">Project Name:</label></div>
+                    <div className="projNameInput"><input type="Text"
                         className="form-control"
                         id="title"
                         aria-describedby="ProjectName"
                         placeholder="Enter Project Name"
                         onChange={handleChange}
-                    />
+                    /></div>
 
                 </div>
                 <div className="form-group text-left">
-                    <label htmlFor="ProjectDescription">Project Description</label>
-                    <textarea type="text"
+                    <div className="projDesc"><label htmlFor="ProjectDescription">Project Description:</label></div>
+                    <div className="projDescInput"><textarea type="text"
                         className="form-control"
                         id="description"
                         placeholder="Project Description"
                         onChange={handleChange}
-                    />
+                    /></div>
                 </div>
                 <div className="form-group text-left">
-                    <label htmlFor="Technologies">Technologies</label>
-                    <textarea type="Text"
+                    <div className="tech"><label htmlFor="Technologies">Technologies:</label></div>
+                    <div className="techInput"><textarea type="Text"
                         className="form-control"
                         id="technologies"
-                        placeholder="Enter technologies you would like to use on project"
+                        placeholder="Enter Technologies Used in Project"
                         onChange={handleChange}
-                    />
+                    /></div>
                 </div>
                 <div className="form-group text-left">
-                    <label htmlFor="Technologies">Project Start Date</label>
-                    <textarea type="Text"
+                    <div className="startDate"><label htmlFor="Technologies">Project Start Date:</label></div>
+                    <div className="startDateInput"><textarea type="Text"
                         className="form-control"
                         id="startDate"
-                        placeholder="Enter technologies you would like to use on project"
+                        placeholder="Enter Project Start Date"
                         onChange={handleChange}
-                    />
+                    /></div>
                 </div>
                 <div className="form-group text-left">
-                    <label htmlFor="Technologies">Project End Date</label>
-                    <textarea type="Text"
+                    <div className="endDate"><label htmlFor="Technologies">Project End Date:</label></div>
+                    <div className="endDateInput"><textarea type="Text"
                         className="form-control"
                         id="endDate"
-                        placeholder="Enter technologies you would like to use on project"
+                        placeholder="Enter Project End Date"
                         onChange={handleChange}
-                    />
+                    /></div>
                 </div>
                 <div className="form-group text-left">
-                    <label htmlFor="Technologies">Status</label>
-                    <input
+                    <div className="status"><label htmlFor="Technologies">Status:</label></div>
+                    <div className="statusInput"><input
                         type="checkbox"
                         name="isChecked"
                         id="open"
                         checked={state.open}
                         onChange={handleChange}
-                    />
+                    /></div>
                 </div>
                 <div className="form-group text-left">
-                    <label htmlFor="Technologies">Team Size</label>
-                    <textarea type="Text"
+                    <div className="size"><label htmlFor="Technologies">Team Size:</label></div>
+                    <div className="sizeInput"><textarea type="Text"
                         className="form-control"
                         id="size"
-                        placeholder="Enter technologies you would like to use on project"
+                        placeholder="Enter Number of Team Members"
                         onChange={handleChange}
-                    />
+                    /></div>
                 </div>
                 <div className="form-group text-left">
-                    <label htmlFor="Technologies">Skills Wanted</label>
-                    <textarea type="Text"
+                    <div className="skills"><label htmlFor="Technologies">Skills Wanted:</label></div>
+                    <div className="skillsInput"><textarea type="Text"
                         className="form-control"
                         id="skills"
-                        placeholder="Enter technologies you would like to use on project"
+                        placeholder="Enter Skills Needed for the Project"
                         onChange={handleChange}
-                    />
+                    /></div>
                 </div>
+                <br />
                 <button
                     type="submit"
                     className="btn btn-primary"
                     onClick={handleFormSubmit}
                 >
-                    Create project
+                    Create Project
                 </button>
-            </div>
 
+            </div>
+            <br />
         </form>
     )
 }
