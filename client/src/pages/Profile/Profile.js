0993bfed3020997dda4f741d0610742aa2a796f4
HRush <hrkoren@gmail.com>
2021-07-07 23:34:47 -0400

    working on pulling project title into profile

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -10,7 +10,7 @@ function Profile() {
     const [stars, setStars] = useState([]);
     const [avgRating, setAvgRating] = useState([]);
     const [users, setUsers] = useState();
-    // const [userProjects, setProjects] = useState([]);
+    const [title, setProjects] = useState([]);
 
     useEffect(() => {
         API.getUsers(users)
@@ -18,11 +18,11 @@ function Profile() {
             .catch(err => console.log(err));
     }, [])
 
-    // useEffect(() => {
-    //     API.getProjects(userProjects)
-    //         .then(res => setProjects(res.data))
-    //         .catch(err => console.log(err));
-    // }, [])
+    useEffect(() => {
+        API.getProjects(title)
+            .then(res => setProjects(res.data))
+            .catch(err => console.log(err));
+    }, [])
 
     const ratingChanged = (newRating) => {
         let average = (arr) => Math.round(arr.reduce((a, b) => a + b) / arr.length);
@@ -77,8 +77,9 @@ function Profile() {
             <section id="userProjects">
                 <div className="set"><strong>Projects</strong></div>
                 <ul className="list">
-                    {user && user.projects ? user.projects.map((data, _id) => {
-                        return <li key={data._id}>{data.title}</li>
+                    {user && user.projects ? user.projects.map((projects, _id) => {
+                        console.log(user.projects)
+                        return <li key={projects._id}>{projects.title}</li>
                     }) : 'loading'}
                 </ul>
                 <div className="set"><strong>Managed Projects</strong></div>
